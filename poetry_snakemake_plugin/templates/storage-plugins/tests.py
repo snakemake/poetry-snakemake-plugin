920825f5d5554362dc12a05fe1548d3527bce53c
Johannes Koester <johannes.koester@uni-due.de>
2023-11-23 14:15:47 +0100

    fix: improve templates

--- poetry_snakemake_plugin/templates/storage-plugins/tests.py
+++ poetry_snakemake_plugin/templates/storage-plugins/tests.py
@@ -4,16 +4,18 @@ from snakemake_interface_storage_plugins.storage_provider import StorageProvider
 from snakemake_interface_storage_plugins.settings import StorageProviderSettingsBase
 
 
-class TestStorageNoSettings(TestStorageBase):
+class TestStorage(TestStorageBase):
     __test__ = True
     retrieve_only = False  # set to True if the storage is read-only
+    store_only = False  # set to True if the storage is write-only
+    delete = True  # set to False if the storage does not support deletion
 
-    def get_query(self) -> str:
+    def get_query(self, tmp_path) -> str:
         # Return a query. If retrieve_only is True, this should be a query that
         # is present in the storage, as it will not be created.
         ...
 
-    def get_query_not_existing(self) -> str:
+    def get_query_not_existing(self, tmp_path) -> str:
         ...
 
     def get_storage_provider_cls(self) -> Type[StorageProviderBase]:
