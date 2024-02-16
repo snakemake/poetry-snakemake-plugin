91da3d3d4c992eb48dcd3a350717388dfc97a57b
Johannes Köster <johannes.koester@uni-due.de>
2024-02-16 22:42:42 +0100

    feat: support for report plugins (#18)

--- poetry_snakemake_plugin/__init__.py
+++ poetry_snakemake_plugin/__init__.py
@@ -3,6 +3,7 @@ from poetry.plugins.application_plugin import ApplicationPlugin
 from poetry_snakemake_plugin.executor_plugins import (
     ScaffoldSnakemakeExecutorPluginCommand,
 )
+from poetry_snakemake_plugin.report_plugins import ScaffoldSnakemakeReportPluginCommand
 from poetry_snakemake_plugin.storage_plugins import (
     ScaffoldSnakemakeStoragePluginCommand,
 )
@@ -17,4 +18,8 @@ class ScaffoldSnakemakeExecutorPlugin(ApplicationPlugin):
         application.command_loader.register_factory(
             ScaffoldSnakemakeStoragePluginCommand.name,
             ScaffoldSnakemakeStoragePluginCommand,
+        )
+        application.command_loader.register_factory(
+            ScaffoldSnakemakeReportPluginCommand.name,
+            ScaffoldSnakemakeReportPluginCommand,
         )
