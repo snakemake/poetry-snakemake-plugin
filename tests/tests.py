4f6b5de95b0b566f192ace5ff1a2d51a16a4bb1d
Johannes Koester <johannes.koester@uni-due.de>
2025-03-02 12:48:19 +0100

    generalize testing

--- tests/tests.py
+++ tests/tests.py
@@ -1,40 +1,26 @@
 import os
+from pathlib import Path
 import subprocess as sp
 
+import pytest
 
-def test_new_snakemake_executor_plugin(tmp_path):
+
+PLUGIN_TYPES = [
+    path.name.removesuffix("-plugins")
+    for path in Path("poetry_snakemake_plugin/templates/").glob("*-plugins")
+]
+
+
+@pytest.mark.parameterize("plugin_type", PLUGIN_TYPES)
+def test_new_plugin(plugin_type, tmp_path):
     orig_dir = os.getcwd()
     os.chdir(tmp_path)
 
-    run_subcommand("new", "snakemake-executor-plugin-test")
-    os.chdir("snakemake-executor-plugin-test")
-    run_subcommand("scaffold-snakemake-executor-plugin")
+    plugin_type = f"{plugin_type}-plugin"
 
-    run_subcommand("run", "black", "--check", "--diff", ".")
-    run_subcommand("run", "flake8")
-    os.chdir(orig_dir)
-
-
-def test_new_snakemake_storage_plugin(tmp_path):
-    orig_dir = os.getcwd()
-    os.chdir(tmp_path)
-
-    run_subcommand("new", "snakemake-storage-plugin-test")
-    os.chdir("snakemake-storage-plugin-test")
-    run_subcommand("scaffold-snakemake-storage-plugin")
-
-    run_subcommand("run", "black", "--check", "--diff", ".")
-    run_subcommand("run", "flake8")
-    os.chdir(orig_dir)
-
-
-def test_new_snakemake_report_plugin(tmp_path):
-    orig_dir = os.getcwd()
-    os.chdir(tmp_path)
-
-    run_subcommand("new", "snakemake-report-plugin-test")
-    os.chdir("snakemake-report-plugin-test")
-    run_subcommand("scaffold-snakemake-report-plugin")
+    run_subcommand("new", f"snakemake-{plugin_type}-test")
+    os.chdir("snakemake-{plugin_type}-test")
+    run_subcommand("scaffold-snakemake-{plugin_type}")
 
     run_subcommand("run", "black", "--check", "--diff", ".")
     run_subcommand("run", "flake8")
